496ed1520bda50807512a84acb587a65d455183f
sz277 <sz277@duke.edu>
2022-12-04 01:56:12 -0500

    NPM install pt2

--- ui/src/views/CustomerScreen.vue
+++ ui/src/views/CustomerScreen.vue
@@ -2,18 +2,73 @@
   <div class="mx-3 my-3">
     <h2>Orders</h2>
     <b-button @click="refresh" class="mb-2">Refresh</b-button>
-    <b-table v-if="customer" :items="customer.orders" />
-    
-    <h2>Draft Order</h2>
-    Check the ingredients you want:
-    <b-form-checkbox-group v-model="draftOrderIngredients" :options="possibleIngredients" />
-    <div class="mt-2">
-      <b-button @click="save">Save</b-button>
-    </div>
-    <div class="mt-2">
-      <b-button @click="submit">Submit</b-button>
-      Note: must save before submitting
-    </div>
+    <b-container fluid class="my-4">
+      <b-row>
+        <b-col xs="12" sm="4">
+          <b-card no-body class="mb-3">
+            <template #header>
+              <div class="d-flex justify-content-between align-items-center">
+                Lists
+                <b-button class="ml-3" size="sm" @click="refreshLists"><b-icon-arrow-clockwise /></b-button>
+              </div>
+            </template>
+            <b-list-group flush>
+              <b-list-group-item
+                v-for="list, i in lists"
+                :key="i"
+                class="d-flex justify-content-between align-items-center"
+                :class="{ 'font-weight-bold': selectedList?.id === list.id }"
+              >
+                <span @click="selectList(list.id)" title="list.id">{{ list.name }}</span>
+                <b-badge variant="dark" pill>{{ list.count }}</b-badge>
+              </b-list-group-item>
+              <b-list-group-item>
+                <b-input-group>
+                  <b-form-input v-model="nameOfListToCreate" placeholder="List name" />
+                  <b-input-group-append>
+                    <b-button @click="handleClickAddList"><b-icon-plus-circle /></b-button>
+                  </b-input-group-append>
+                </b-input-group>
+              </b-list-group-item>
+            </b-list-group>
+          </b-card>
+        </b-col>
+        <b-col xs="12" sm="8">
+          <b-card no-body>
+            <template #header>
+              <div v-if="selectedList != null" class="d-flex justify-content-between align-items-center">
+                {{ selectedList.name }}
+                <b-button class="ml-3" size="sm" @click="refreshSelectedList"><b-icon-arrow-clockwise /></b-button>
+              </div>
+              <div v-else>
+                No List Selected
+              </div>
+            </template>
+            <b-list-group flush>
+              <b-list-group-item
+                v-for="item, i in (selectedList == null ? [] : selectedList.items)"
+                :key="i"
+                class="d-flex justify-content-between align-items-center"
+              >
+                <span :title="item.id">
+                  <b-form-checkbox class="d-inline-block" @input="checkItem(item.id, $event)" :checked="item.completed" />
+                  {{ item.description }}
+                </span>
+                <b-badge variant="secondary">{{ item.priority }}</b-badge>
+              </b-list-group-item>
+              <b-list-group-item v-if="selectedList != null">
+                <b-input-group>
+                  <b-form-input v-model="descriptionOfItemToAdd" placeholder="Task description" />
+                  <b-input-group-append>
+                    <b-button @click="handleClickAddItem"><b-icon-plus-circle /></b-button>
+                  </b-input-group-append>
+                </b-input-group>
+              </b-list-group-item>
+            </b-list-group>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -28,7 +83,6 @@ const draftOrderIngredients: Ref<string[]> = ref([])
 const possibleIngredients: Ref<string[]> = ref([])
 
 async function refresh() {
-  possibleIngredients.value = await (await fetch("/api/possible-ingredients")).json()
 
   if (user.value) {
     customer.value = await (await fetch("/api/customer")).json()
