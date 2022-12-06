257fc99535dbe7f3ccd53c77ce9ad36499247643
sz277 <sz277@duke.edu>
2022-12-06 06:07:32 -0500

    Finished E2E

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -8,10 +8,10 @@ test('homepage has Playwright in title and get started link linking to the intro
   await page.locator('input[id="password"]').type("12345")
   await page.locator('input[id="kc-login"]').click()
 
-  await page.click("text='Create a Post'")
+  await page.goto(process.env.APP_URL || 'http://127.0.0.1:8082/posting')
   await page.locator('input[id="title-input"]').type("hello")
   await page.locator('textarea[id="content-input"]').type("I'm a dog")
   await page.click("text='Submit'")
-  await page.locator("'text='hello''").click()
+  page.locator('"hello"').click()
   await expect(page.locator('h2')).toHaveText("hello")
 });
